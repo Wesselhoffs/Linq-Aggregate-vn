5c7d412c3cafe9d36d95b79fb0c3a207d86dabae
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-10-11 12:57:14 +0200

    Fixade errors

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿
+﻿using System.Text;
 
 string[] techniques = { "   C#", "daTAbaser", "WebbuTVeCkling ", "clean Code   " };
 
 string kurser = "";
+kurser = courseGenerator(techniques);
 
 string courseGenerator(string[] techniques)
 {
@@ -14,25 +15,35 @@ string courseGenerator(string[] techniques)
     }
     return kurser;
 }
+Console.WriteLine(kurser);
 
+Console.WriteLine("----------------------------------");
 //Diskutera hur det ska gå till och skriv om denna så att
 //den använder en Aggregator.
 
-//string[] newArray = techniques.Aggregate("", (test, next) => next.Trim()List<Monster> monsterList = new List<Monster>(){new Monster("Gromp", 100, false),
+
+string fixedTechniques = techniques.Aggregate("", (a, b) =>
+{
+    b = b.Trim();
+    b = "<p>" + b[0].ToString().ToUpper() + b.Substring(1).ToLower() + "</p>\n";
+    return a + b;
+});Console.WriteLine(fixedTechniques);Console.WriteLine("----------------------------------");
+List<Monster> monsterList = new List<Monster>(){new Monster("Gromp", 100, false),
                                                 new Monster("Cranky", 120, true),
                                                 new Monster("CoffeeSpiller", 35, false)};
-      //1.Använd SELECT och FOREACH för att skriva ut alla namn på monstrena (det är en bra
+
+//1.Använd SELECT och FOREACH för att skriva ut alla namn på monstrena (det är en bra
 //ide att lägga in alla monsternamn i en var eller List<string> om ni vill ha en
 //temporär variabel).
 
 monsterList.Select(m => m.Name).ToList().ForEach(m => Console.WriteLine(m));
 
+// Eller
 
+var monsterNames = from monster in monsterList
+                   select monster.Name;
 
-var tempVar = from monster in monsterList
-              select monster.Name;
-
-foreach (var name in tempVar)
+foreach (var name in monsterNames)
 {
     Console.WriteLine(name);
 }
@@ -44,9 +55,12 @@ Console.WriteLine("---------------------------------");
 //health.
 monsterList.Where(m => m.Health > 50).ToList().ForEach(monster => Console.WriteLine(monster.Name + " " + monster.Health));
 
+// Eller
+
 var monsterWithHealtoverFifty = from monster in monsterList
                                 where monster.Health > 50
                                 select monster;
+
 foreach (var monster in monsterWithHealtoverFifty)
 {
     Console.WriteLine($"{monster.Name} {monster.Health}");
@@ -59,6 +73,8 @@ Console.WriteLine("---------------------------------");
 //döda.
 
 monsterList.Where(m => m.isDead == false).ToList().ForEach(m => Console.WriteLine(m.Name + " is not dead"));
+
+// Eller
 
 var notDead = from monster in monsterList
               where monster.isDead == false
@@ -78,9 +94,11 @@ Console.WriteLine("---------------------------------");
 monsterList.OrderBy(m => m.Name).ToList().ForEach(m => Console.WriteLine(m.Name));
 monsterList.OrderByDescending(m => m.Name).ToList().ForEach(m => Console.WriteLine(m.Name));
 
+// Eller
+
 var ascendingMonsters = from monster in monsterList
-                      orderby monster.Name ascending
-                      select monster;
+                        orderby monster.Name ascending
+                        select monster;
 
 var descendingMonsters = from monster in monsterList
                          orderby monster.Name descending
